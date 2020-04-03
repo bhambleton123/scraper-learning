f3036650ebd2f854dd23f0b699bce445bf4becae
Brian Hambleton <bhambl3t0n1@gmail.com>
2020-04-03 08:07:59 -0700

    Started working on ebay scraper, made it easier to add more scrapers

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const controllers = require("./controllers");
 const port = 3001;
 
-app.get("/scrape", controllers.scrapeAmazon);
+app.get("/scrape", controllers.scrapeWebsite);
 app.get("/test", controllers.testAvoidDetection);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
